1786aee907b2c36ed34d5961ba6705f6b2f41825
Naiqing624 <naiqing.cao24@gmail.com>
2025-04-27 13:24:10 +0200

    ex00 done

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/27 11:24:29 by marvin            #+#    #+#             */
+/*   Updated: 2025/04/27 11:24:29 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat 
+{
+	
+	private:
+	const std::string	_name;
+	int					_grade;
+
+	public:
+	Bureaucrat(const std::string name, int grade);
+	~Bureaucrat();
+	Bureaucrat(const Bureaucrat &other);
+	Bureaucrat &operator = (const Bureaucrat &other);
+	
+	const std::string	&getName() const;
+	int	getGrade() const;
+	void	increGrade(int num);
+	void	decreGrade(int num);
+
+	class GradeTooHighException : public std::exception 
+	{
+		public:
+		const char *what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception 
+	{
+		public:
+		const char *what() const throw();
+	};
+};
+
+std::ostream &operator << (std::ostream &out, const Bureaucrat &bureaucrat);
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/27 11:24:08 by marvin            #+#    #+#             */
+/*   Updated: 2025/04/27 11:24:08 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int	main(void)
+{
+	try
+	{
+		Bureaucrat	p1("kekel", 50);
+		std::cout << p1 << std::endl;
+		// Bureaucrat	p2("kekel2", 550);
+		// std::cout << p2 << std::endl;
+		Bureaucrat	p3(p1);
+		std::cout << p3 << std::endl;
+		Bureaucrat	p4("kekel4", -5);
+		std::cout << p4 << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
